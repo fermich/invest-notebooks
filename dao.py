498a42f26834b6ee630e69c247f24843fde0dbb5
Michal Ferlinski <fermich@gmail.com>
2017-06-24 13:37:19 +0200

    fixes

--- dao.py
+++ dao.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 class InvestDao:
-    def __init__(self, host, port, user, passwd, db):
+    def __init__(self, host="172.17.0.1", port=3306, user="michal", passwd="michal12", db="invest_data"):
         self.conn = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
 
     def sql(self, query):
